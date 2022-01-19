5dfd488317ba7af23bba758e965e2065153b0d25
patrick <patrickandersonn29@gmail.com>
2022-01-19 14:18:03 -0300

    Mantendo os itens do localstorage na lista

--- projeto-inicial/projeto_inicial/componentes/criaTarefa.js
+++ projeto-inicial/projeto_inicial/componentes/criaTarefa.js
@@ -6,7 +6,6 @@ import BotaoDeleta from "./deletaTarefa.js"
 
     const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
     
-    const lista = document.querySelector("[data-list]")
     const input = document.querySelector("[data-form-input]")    
     const valor = input.value
     
@@ -23,9 +22,6 @@ import BotaoDeleta from "./deletaTarefa.js"
 
     const tarefasAtualizadas = [... tarefas, dados]
 
-    const criaTarefa = Tarefa (dados)
-
-    lista.appendChild(criaTarefa)
 
     localStorage.setItem("tarefas",JSON.stringify(tarefasAtualizadas))
 
